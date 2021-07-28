04b51804df08f9c807e10a8c789a6a6d0246a3aa
hyeju <hae7077@naver.com>
2021-07-28 12:29:37 +0900

    백준 1912번 연속합 - DP

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int dp[];
+    static int arr[];
+   public static void main(String args[]) throws Exception{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st;
+       int n = Integer.parseInt(br.readLine());
+       dp = new int[n];
+       arr = new int[n];
+       st = new StringTokenizer(br.readLine());
+       for (int i = 0; i < n; i++) {
+           arr[i] = Integer.parseInt(st.nextToken());
+       }
+       dp[0] = arr[0];
+       int max = arr[0];
+       for (int i = 1; i < n; i++) {
+           dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
+           max = Math.max(max, dp[i]);
+       }
+       System.out.println(max);
+   }
+}
