401611c7add01c734027be0e3d690111612e7368
hyeju <hae7077@naver.com>
2021-10-01 16:23:17 +0900

    10814 나이순 정렬 - 정렬

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[][] arr = new String[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = scanner.next();
+            arr[i][1] = scanner.next();
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            return Integer.parseInt(e1[0]) - Integer.parseInt(e2[0]);
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
