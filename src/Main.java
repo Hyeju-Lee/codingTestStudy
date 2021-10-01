57685df837ec5a0ba5b7e6ae74d2eb85e1309a2f
hyeju <hae7077@naver.com>
2021-10-01 16:15:32 +0900

    11650 좌표 정렬하기 - 정렬

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = scanner.nextInt();
+            arr[i][1] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] e1, int[] e2) {
+                if (e1[0] == e2[0]) {
+                    return e1[1] - e2[1];
+                }
+                else {
+                    return e1[0] - e2[0];
+                }
+            }
+        });
+
+        /* 람다식 표현
+       Arrays.sort(arr, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            }
+            else {
+                return e1[0] - e2[0];
+            }
+        });*/
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
