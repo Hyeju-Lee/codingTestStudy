399deec8c8a965ab1cd7fcede90a67a4f30b52a4
hyeju <hae7077@naver.com>
2022-05-31 14:21:10 +0900

    프로그래머스 hash - 완주하지 못한 선수

---
+++ src/Programmers/Hash_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Hash_1 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for(String player : participant) hashMap.put(player, hashMap.getOrDefault(player, 0) + 1);
+        for (String player : completion) hashMap.put(player, hashMap.get(player) - 1);
+
+        for (String key : hashMap.keySet()) {
+            if (hashMap.get(key) != 0) {
+                answer = key;
+            }
+        }
+        return answer;
+    }
+}
